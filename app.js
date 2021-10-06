1b9206061ef8222419034aee4b34e3ffea50a4b1
michaelsatumba <mjsatumba@gmail.com>
2021-10-06 11:34:26 -0700

    gitignore

--- app.js
+++ app.js
@@ -75,8 +75,12 @@ app.get("/posts/:postName", function(req, res){
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started successfully.");
 });
