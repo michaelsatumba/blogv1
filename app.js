ca596ea77f81bcd39807ec54ed2bda6ec35ab7de
michaelsatumba <mjsatumba@gmail.com>
2021-10-06 11:43:29 -0700

    delete app.listen(port)

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
-app.listen(port);
+
 
 app.listen(port, function() {
   console.log("Server started successfully.");
